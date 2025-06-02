db9a6c9efc777469854509ee78b64b5eafb68dee
i2bskn <i2bskn@gmail.com>
2025-06-03 07:24:24 +0900

    update

--- lib/sendgrid/helpers/mail/personalization.rb
+++ lib/sendgrid/helpers/mail/personalization.rb
@@ -82,7 +82,7 @@ module SendGrid
       additional_email = addition.email.downcase
 
       [@tos, @ccs, @bccs].flatten.each do |elm|
-        return true if elm && elm.dig('email') && elm.dig('email').downcase == additional_email
+        return true if elm && elm['email'] && elm['email'].downcase == additional_email
       end
 
       false
